8bc80c4477df45bf86bfaaa743131ce9b1456ec0
Ani <205335742+Ani0o@users.noreply.github.com>
2025-11-30 18:12:15 +0530

    changed nav bar in index.html and changed username to Email in login.html and script.js

--- public/Script.js
+++ public/Script.js
@@ -2,7 +2,7 @@
 if (document.getElementById('loginForm')) {
     document.getElementById('loginForm').addEventListener('submit', async function(event) {
         event.preventDefault();
-        const email = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         if (email && password) {
             try {
