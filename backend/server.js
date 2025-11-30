e9cec07db80317795d636d51879dcc748556aaf5
Ani <205335742+Ani0o@users.noreply.github.com>
2025-11-30 18:47:51 +0530

    fixed port in server.js for redeploy

--- backend/server.js
+++ backend/server.js
@@ -143,7 +143,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/Index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
